26bdd1492307849eae13aed4beca4cfa1981ddb4
Gregg <arkhipov.zena@gmail.com>
2022-12-04 22:06:43 +0500

    UWU

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,41 @@
 package com.example.task01;
 
-public class Pair {
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class Pair<F,S> {
+    final private F first;
+    final private S second;
+    private Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+    public F getFirst() {
+        return first;
+    }
+    public S getSecond() {
+        return second;
+    }
+
+    public boolean equals(Pair pair){
+        if (pair.getFirst() == first && pair.getSecond() == second)
+            return true;
+        else
+            return false;
+    }
+    public static <F,S> Pair<F,S> of(F first, S second) {
+        return new Pair<F,S>(first, second);
+    }
+    public void ifPresent(BiConsumer consumer) {
+        if(first != null && second != null){
+            consumer.accept(first, second);
+        }
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
     // TODO напишите реализацию
 }
--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,69 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SavedList<E extends Serializable> extends AbstractList<E> {
-
+public class SavedList<E extends Serializable> extends AbstractList<E> implements Serializable {
+    private List<E> elements = new ArrayList<>();
+    private File file;
     public SavedList(File file) {
+        this.file = file;
+        if (file.exists()) {
+            Path path = file.toPath();
+            List<E> elements;
+            try (ObjectInputStream oos = new ObjectInputStream(Files.newInputStream(path))) {
+                elements = (List) oos.readObject();
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            this.elements = elements;
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return elements.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E save = elements.get(index);
+        elements.set(index, element);
+        serialization();
+        return save;
     }
 
     @Override
     public int size() {
-        return 0;
+        return elements.size();
     }
 
     @Override
     public void add(int index, E element) {
+        elements.add(index, element);
+        serialization();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E save = elements.get(index);
+        elements.remove(index);
+        serialization();
+        return save;
     }
+    private void serialization() {
+        Path path = file.toPath();
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))){
+            oos.writeObject(elements);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
